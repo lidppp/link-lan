ffbf7f3735128c4dc10aa4528ccabf1aedcecf6b
lidppp <1512282028@qq.com>
2024-03-22 17:51:17 +0800

    init project

--- controller/index.js
+++ controller/index.js
@@ -1,1 +1,26 @@
-export default {}
+import Express from "express"
+const router = Express.Router()
+
+router.route("/api/message")
+    .get((req, res)=>{
+      res.send("message get")
+    })
+    .post((req,res)=>{
+      res.send("message post")
+    })
+    .delete((req, res)=>{
+      res.send("message delete")
+    })
+
+router.route("/api/file")
+    .get((req, res)=>{
+      res.send("file get")
+    })
+    .post((req,res)=>{
+      res.send("file post")
+    })
+    .delete((req, res)=>{
+      res.send("file delete")
+    })
+
+export default router
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "node:path";
 import {fileURLToPath} from 'url';
 import history from "connect-history-api-fallback"
-
+import router from "./controller/index.js"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,17 +12,12 @@ const port = 3000
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get("/api", function (req, res) {
-  res.send("hello world")
-})
-
-
-
+app.use(router)
 
 app.use(history());
 
 app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname , './dist/index.html'))
+  res.sendFile(path.join(__dirname, './dist/index.html'))
 })
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`)
